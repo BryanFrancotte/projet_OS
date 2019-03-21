ebac6d6249d05f636665efc7d56a7b0a63916564
Bryan <francotte.bryan@gmai.com>
2019-03-21 16:50:48 +0100

    did the find in the /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin variable

--- client.sh
+++ client.sh
@@ -1,3 +1,8 @@
 echo -n "Introduire une expression pour la commande à exécuter : "
 read inputRegEx
 echo $inputRegEx
+
+for directory in $(echo $PATH | tr ':' '\n'); do 
+#Divide directories in $PATH to one directory each line
+	find $directory -name "$inputRegEx"
+done
